3675faf0634034dbe5f014125eaa9e2b09dc1b2f
yasincandev <canyasin1996@gmail.com>
2022-08-25 21:41:33 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import mobilePattern from "./images/pattern-divider-mobile.svg";
+import desktopPattern from "./images/pattern-divider-desktop.svg";
+import dice from "./images/icon-dice.svg";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const advice = await axios("https://api.adviceslip.com/advice");
+    const response = advice.data;
+    setAdvice(response.slip);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <span className="advice-id">Advice #{advice.id}</span>{" "}
+      <h1 className="advice">{advice.advice}</h1>{" "}
+      <div className="divider">
+        <img src={desktopPattern} className="desktop" />
+        <img src={mobilePattern} className="mobile" />
+      </div>
+      <button className="btn" onClick={getAdvice}>
+        <img src={dice} alt="" />
+      </button>
     </div>
   );
 }
